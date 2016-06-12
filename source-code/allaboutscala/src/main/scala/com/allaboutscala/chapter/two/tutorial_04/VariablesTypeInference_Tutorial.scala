7c859260a1ad5c72fcfa1cc139f69a820ee113c5
Nadim Bahadoor <nbahadoor@gmail.com>
2016-06-13 00:01:50 +0100

    Adding source code for Chapter 2, Tutorial 4, An Overview Of Scala Type Inference.

--- source-code/allaboutscala/src/main/scala/com/allaboutscala/chapter/two/tutorial_04/VariablesTypeInference_Tutorial.scala
+++ source-code/allaboutscala/src/main/scala/com/allaboutscala/chapter/two/tutorial_04/VariablesTypeInference_Tutorial.scala
@@ -3,9 +3,9 @@ package com.allaboutscala.chapter.two.tutorial_04
 /**
   * Created by nbahadoor on 10/06/2016.
   *
-  * Tutorial:
+  * Tutorial: An Overview Of Scala Type Inference
   *
-  * 
+  * [[http://allaboutscala.com/tutorials/chapter-2-learning-basics-scala-programming/scala-tutorial-overview-scala-type-inference/ Tutorial]]
   *
   * Copyright 2016 Nadim Bahadoor (http://allaboutscala.com)
   *
@@ -24,6 +24,44 @@ package com.allaboutscala.chapter.two.tutorial_04
 object VariablesTypeInference_Tutorial extends App {
 
 
+  println("Step 1: Immutable variable")
+  val donutsToBuy: Int = 5
+
+  val donutsBought = 5
 
 
+
+  println("\nStep 2: Scala Types")
+  val donutsBoughtToday = 5
+
+  val bigNumberOfDonuts = 100000000L
+
+  val smallNumberOfDonuts = 1
+
+  val priceOfDonut = 2.50
+
+  val donutPrice = 2.50f
+
+  val donutStoreName = "Allaboutscala Donut Store"
+
+  val donutByte = 0xa
+
+  val donutFirstLetter = 'D'
+
+  val nothing = ()
+
+
+
+  println("\nStep 3: Using Scala compiler to convert from one data type to another")
+  val numberOfDonuts: Short = 1
+
+  val minimumDonutsToBuy: Int = numberOfDonuts
+
+
+
+  println("\nStep 4: User driven conversion from one data type to another ")
+  // NB: You cannot convert from an Int to a String
+  // val minimumDonutsToSell: String = numberOfDonuts
+
+  val minimumDonutsToSell: String = numberOfDonuts.toString()
 }
