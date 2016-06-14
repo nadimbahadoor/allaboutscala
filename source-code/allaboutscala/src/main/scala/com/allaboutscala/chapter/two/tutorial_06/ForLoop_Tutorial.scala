df5339cd1af6d7c5003b8efd9b593147fb6cc8bd
Nadim Bahadoor <nbahadoor@gmail.com>
2016-06-15 00:17:31 +0100

    Adding source code for Chapter 2, Tutorial 6, Learn How To Use For Comprehension.

---
+++ source-code/allaboutscala/src/main/scala/com/allaboutscala/chapter/two/tutorial_06/ForLoop_Tutorial.scala
@@ -0,0 +1,71 @@
+package com.allaboutscala.chapter.two.tutorial_06
+
+/**
+  * Created by nbahadoor on 14/06/2016.
+  *
+  * Tutorial: Learn How To Use For Comprehension
+  *
+  * [[http://allaboutscala.com/tutorials/chapter-2-learning-basics-scala-programming/scala-tutorial-learn-use-for-comprehension/ Tutorial]]
+  *
+  * Copyright 2016 Nadim Bahadoor (http://allaboutscala.com)
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  * use this file except in compliance with the License. You may obtain a copy of
+  * the License at
+  *
+  *  [http://www.apache.org/licenses/LICENSE-2.0]
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+  * License for the specific language governing permissions and limitations under
+  * the License.
+  */
+object ForLoop_Tutorial extends App {
+
+
+  println("Step 1: A simple for loop from 1 to 5 inclusive")
+  for(numberOfDonuts <- 1 to 5){
+    println(s"Number of donuts to buy = $numberOfDonuts")
+  }
+
+
+
+  println("\nStep 2: A simple for loop from 1 to 5, where 5 is NOT inclusive")
+  for(numberOfDonuts <- 1 until 5){
+    println(s"Number of donuts to buy = $numberOfDonuts")
+  }
+
+
+
+  println("\nStep 3: Filter values using if conditions in for loop")
+  val donutIngredients = List("flour", "sugar", "egg yolks", "syrup", "flavouring")
+  for(ingredient <- donutIngredients if ingredient == "sugar"){
+    println(s"Found sweetening ingredient = $ingredient")
+  }
+
+
+
+  println("\nStep 4: Filter values using if conditions in for loop and return the result back using the yield keyword")
+  val sweeteningIngredients = for {
+    ingredient <- donutIngredients
+    if (ingredient == "sugar" || ingredient == "syrup")
+  } yield ingredient
+  println(s"Sweetening ingredients = $sweeteningIngredients")
+
+
+
+  println("\nStep 5: Using for comprehension to loop through 2-Dimensional array")
+  val twoDimensionalArray = Array.ofDim[String](2,2)
+  twoDimensionalArray(0)(0) = "flour"
+  twoDimensionalArray(0)(1) = "sugar"
+  twoDimensionalArray(1)(0) = "egg"
+  twoDimensionalArray(1)(1) = "syrup"
+
+  for { x <- 0 until 2
+        y <- 0 until 2
+  } println(s"Donut ingredient at index ${(x,y)} = ${twoDimensionalArray(x)(y)}")
+
+
+
+}
