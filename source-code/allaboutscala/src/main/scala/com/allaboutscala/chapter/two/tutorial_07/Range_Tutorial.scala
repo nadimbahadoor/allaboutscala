a02660ce5ed2d47a8cb4e33c226318443941a3e6
Nadim Bahadoor <nbahadoor@gmail.com>
2016-06-15 23:49:05 +0100

    Adding source code for Chapter 2, Tutorial 7, Learn How To Use Range.

---
+++ source-code/allaboutscala/src/main/scala/com/allaboutscala/chapter/two/tutorial_07/Range_Tutorial.scala
@@ -0,0 +1,76 @@
+package com.allaboutscala.chapter.two.tutorial_07
+
+/**
+  * Created by nbahadoor on 15/06/2016.
+  *
+  * Tutorial: Learn How To Use Range (Inclusive, Exclusive)
+  *
+  * [[http://allaboutscala.com/tutorials/chapter-2-learning-basics-scala-programming/scala-tutorial-learn-use-range-inclusive-exclusive/ Tutorial]]
+  *
+  * Copyright 2016 Nadim Bahadoor (http://allaboutscala.com)
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  * use this file except in compliance with the License. You may obtain a copy of
+  * the License at
+  *
+  *  [http://www.apache.org/licenses/LICENSE-2.0]
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+  * License for the specific language governing permissions and limitations under
+  * the License.
+  */
+object Range_Tutorial extends App {
+
+  println("Step 1: Create a simple numeric range from 1 to 5 inclusive")
+  val from1To5 = 1 to 5
+  println(s"Range from 1 to 5 inclusive = $from1To5")
+
+
+
+  println("\nStep 2: Create a numeric range from 1 to 5 but excluding the last integer number 5")
+  val from1Until5 = 1 until 5
+  println(s"Range from 1 until 5 where 5 is excluded = $from1Until5")
+
+
+
+  println("\nStep 3: Create a numeric range from 0 to 10 but increment with multiples of 2")
+  val from0To10By2 = 0 to 10 by 2
+  println(s"Range from 0 to 10 with multiples of 2 = $from0To10By2")
+
+
+
+  println("\nStep 4: Create an alphabetical range to represent letter a to z")
+  val alphabetRangeFromAToZ = 'a' to 'z'
+  println(s"Range of alphabets from a to z = $alphabetRangeFromAToZ")
+
+
+
+  println(s"\nStep 5: Character ranges with user specified increment")
+  val alphabetRangeFromAToZBy2 = 'a' to 'z' by 2
+  println(s"Range of every other alphabet = $alphabetRangeFromAToZBy2")
+
+
+
+  println("\nStep 6: Storing our ranges into collections")
+  val listFrom1To5 = (1 to 5).toList
+  println(s"Range to list = ${listFrom1To5.mkString(" ")}")
+
+  val setFrom1To5 = (1 to 5).toSet
+  println(s"Range to set = ${setFrom1To5.mkString(" ")}")
+
+  val sequenceFrom1To5 = (1 to 5).toSeq
+  println(s"Range to sequence = ${sequenceFrom1To5.mkString(" ")}")
+
+  val arrayFrom1To5 = (1 to 5).toArray
+  println(s"Range to array = `${arrayFrom1To5.mkString(" ")}")
+
+
+
+  // Tip:
+  // - To print every element in the array variable called arrayFrom1To5
+  arrayFrom1To5.foreach(print(_))
+
+
+}
