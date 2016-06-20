f5375a471d7afeb493e0bd62c69f7c2eb0e42a99
Nadim Bahadoor <nbahadoor@gmail.com>
2016-06-20 23:43:49 +0100

    Adding source code, Chapter 2, Tutorial 11, Learn How To Use Option – Avoid Null

---
+++ source-code/allaboutscala/src/main/scala/com/allaboutscala/chapter/two/tutorial_11/Option_Tutorial.scala
@@ -0,0 +1,53 @@
+package com.allaboutscala.chapter.two.tutorial_11
+
+/**
+  * Created by Nadim Bahadoor on 20/06/2016.
+  *
+  * Tutorial: Learn How To Use Option – Avoid Null
+  *
+  * [[http://allaboutscala.com/tutorials/chapter-2-learning-basics-scala-programming/scala-tutorial-learn-use-option-avoid-null/ Tutorial]]
+  *
+  * Copyright 2016 Nadim Bahadoor (http://allaboutscala.com)
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  * use this file except in compliance with the License. You may obtain a copy of
+  * the License at
+  *
+  *  [http://www.apache.org/licenses/LICENSE-2.0]
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+  * License for the specific language governing permissions and limitations under
+  * the License.
+  *
+  */
+object Option_Tutorial extends App {
+
+
+  println("Step 1: How to use Option and None - a basic example")
+  val glazedDonutTaste: Option[String] = Some("Very Tasty")
+  println(s"Glazed Donut taste = ${glazedDonutTaste.get}")
+
+
+
+  println("\nStep 2: How to use Option and None - a basic example")
+  val glazedDonutName: Option[String] = None
+  println(s"Glazed Donut name = ${glazedDonutName.getOrElse("Glazed Donut")}")
+
+
+
+  println("\nStep 3: How to use Pattern Matching with Option")
+  glazedDonutName match {
+    case Some(name) => println(s"Received donut name = $name")
+    case None       => println(s"No donut name was found!")
+  }
+
+
+
+  println("\nTip 1: Filter None using map function")
+  glazedDonutTaste.map(taste => println(s"glazedDonutTaste = $taste"))
+  glazedDonutName.map(name => println(s"glazedDonutName = $name"))
+
+
+}
