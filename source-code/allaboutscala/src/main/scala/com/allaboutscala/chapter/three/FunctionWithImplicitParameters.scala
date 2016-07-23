b469a36f51f257427cdd2c84115a9c1fb5c5c8c9
Nadim Bahadoor <nbahadoor@gmail.com>
2016-07-23 22:44:05 +0100

    Adding Chapter 3, Tutorial 5, Learn How To Create Function With Implicit Parameter.

---
+++ source-code/allaboutscala/src/main/scala/com/allaboutscala/chapter/three/FunctionWithImplicitParameters.scala
@@ -0,0 +1,64 @@
+package com.allaboutscala.chapter.three
+
+/**
+  * Created by Nadim Bahadoor on 28/06/2016.
+  *
+  *  Tutorial: Learn How To Create Function With Implicit Parameter
+  *
+  * [[http://allaboutscala.com/tutorials/chapter-3-beginner-tutorial-using-functions-scala/scala-tutorial-learn-create-function-implicit-parameter/ Tutorial]]
+  *
+  * Copyright 2016 Nadim Bahadoor (http://allaboutscala.com)
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  * use this file except in compliance with the License. You may obtain a copy of
+  * the License at
+  *
+  *  [http://www.apache.org/licenses/LICENSE-2.0]
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+  * License for the specific language governing permissions and limitations under
+  * the License.
+  */
+object FunctionWithImplicitParameters extends App {
+
+  println(s"Step 1: How to define a function with an implicit parameter")
+  def totalCost(donutType: String, quantity: Int)(implicit discount: Double): Double = {
+    println(s"Calculating the price for $quantity $donutType")
+    val totalCost = 2.50 * quantity * (1 - discount)
+    totalCost
+  }
+
+
+
+  println("\nStep 2: How to define an implicit value")
+  implicit val discount: Double = 0.1
+  println(s"All customer will receive a ${discount * 100}% discount")
+
+
+
+  println("\nStep 3: How to call a function which has an implicit parameter")
+  println(s"""Total cost with discount of 5 Glazed Donuts = ${totalCost("Glazed Donut", 5)}""")
+
+
+
+  println("\nStep 4: How to define a function which takes multiple implicit parameters")
+  def totalCost2(donutType: String, quantity: Int)(implicit discount: Double, storeName: String): Double = {
+    println(s"[$storeName] Calculating the price for $quantity $donutType")
+    val totalCost = 2.50 * quantity * (1 - discount)
+    totalCost
+  }
+
+
+
+  println("\nStep 5: How to call a function which takes multiple implicit parameters")
+  implicit val storeName: String = "Tasty Donut Store"
+  println(s"""Total cost with discount of 5 Glazed Donuts = ${totalCost2("Glazed Donut", 5)}""")
+
+
+
+  println("\nStep 6: How to manually pass-through implicit parameters")
+  println(s"""Total cost with discount of 5 Glazed Donuts, manually passed-through = ${totalCost2("Glazed Donut", 5)(0.1, "Scala Donut Store")}""")
+
+}
