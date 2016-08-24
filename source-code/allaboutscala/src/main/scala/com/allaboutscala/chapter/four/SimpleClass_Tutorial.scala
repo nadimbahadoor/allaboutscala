e3ef02bd21a49d87793dc71751361f71f7e20663
Nadim Bahadoor <nbahadoor@gmail.com>
2016-08-24 21:53:48 +0100

    Adding Chapter 4, Tutorial 1, Learn How To Create Classes And Objects In Scala.

---
+++ source-code/allaboutscala/src/main/scala/com/allaboutscala/chapter/four/SimpleClass_Tutorial.scala
@@ -0,0 +1,53 @@
+package com.allaboutscala.chapter.four
+
+/**
+  * Created by Nadim Bahadoor on 28/06/2016.
+  *
+  *  Tutorial: Learn How To Create Classes And Objects In Scala
+  *
+  * [[http://allaboutscala.com/tutorials/chapter-3-beginner-tutorial-using-classes-scala/scala-tutorial-learn-create-classes-objects/ Tutorial]]
+  *
+  * Copyright 2016 Nadim Bahadoor (http://allaboutscala.com)
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  * use this file except in compliance with the License. You may obtain a copy of
+  * the License at
+  *
+  *  [http://www.apache.org/licenses/LICENSE-2.0]
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+  * License for the specific language governing permissions and limitations under
+  * the License.
+  */
+object SimpleClass_Tutorial extends App {
+
+  println("\nStep 2: How to create instances of Donut class")
+  val glazedDonut = new Donut("Glazed Donut", 1111)
+  val vanillaDonut = new Donut("Vanilla Donut", 2222)
+
+
+
+  println("\nStep 3: How to call the print function for each of the donut object")
+  glazedDonut.print
+  vanillaDonut.print
+
+
+
+  println("\nStep 4: How to access the properties of class Donut")
+// glazedDonut.name
+// glazedDonut.productCode
+  // NOTE:
+  // - You will get compiler error as we have not exposed the name and productCode properties using getters.
+  // - We will introduce Case Class in upcoming tutorials to automatically provide accessor methods.
+}
+
+
+
+// Step 1: How to define a simple class to represent a Donut object")
+class Donut(name: String, productCode: Long) {
+
+  def print = println(s"Donut name = $name, productCode = $productCode")
+
+}
