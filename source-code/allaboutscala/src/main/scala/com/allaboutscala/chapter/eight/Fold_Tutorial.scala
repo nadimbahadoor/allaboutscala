0308067fc30c29c97732cdcd8b804c529927998d
Nadim Bahadoor <nbahadoor@gmail.com>
2016-11-27 20:57:53 +0000

    Adding Chapter 8, Learn How To Use Fold Function With Examples.

---
+++ source-code/allaboutscala/src/main/scala/com/allaboutscala/chapter/eight/Fold_Tutorial.scala
@@ -0,0 +1,57 @@
+package com.allaboutscala.chapter.eight
+
+/**
+  * Created by Nadim Bahadoor on 28/06/2016.
+  *
+  *  Tutorial: Learn How To Use Fold Function With Examples
+  *
+  * [[http://allaboutscala.com/tutorials/chapter-8-beginner-tutorial-using-scala-collection-functions/scala-fold-example/ Tutorial]]
+  *
+  * Copyright 2016 Nadim Bahadoor (http://allaboutscala.com)
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  * use this file except in compliance with the License. You may obtain a copy of
+  * the License at
+  *
+  *  [http://www.apache.org/licenses/LICENSE-2.0]
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+  * License for the specific language governing permissions and limitations under
+  * the License.
+  */
+object Fold_Tutorial extends App {
+
+  println("Step 1: How to initialize a sequence of donut prices")
+  val prices: Seq[Double] = Seq(1.5, 2.0, 2.5)
+  println(s"Donut prices = $prices")
+
+
+
+  println("\nStep 2: How to sum all the donut prices using fold function")
+  val sum = prices.fold(0.0)(_ + _)
+  println(s"Sum = $sum")
+
+
+
+  println("\nStep 3: How to initialize a Sequence of donuts")
+  val donuts: Seq[String] = Seq("Plain", "Strawberry", "Glazed")
+  println(s"Elements of donuts1 = $donuts")
+
+
+
+  println("\nStep 4: How to create a String of all donuts using fold function")
+  println(s"All donuts = ${donuts.fold("")((a, b) => a + b + " Donut ")}")
+
+
+
+  println("\nStep 5: How to declare a value function to create the donut string")
+  val concatDonuts: (String, String) => String = (a, b) => a + b + " Donut "
+  println(s"Value function concatDonuts = $concatDonuts")
+
+
+
+  println("\nStep 6: How to create a String of all donuts using value function from Step 5 and fold function")
+  println(s"All donuts = ${donuts.fold("")(concatDonuts)}")
+}
