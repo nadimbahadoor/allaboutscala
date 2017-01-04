fd010cc0a6c4ce8256d0459172308b5188a6e3b4
Nadim Bahadoor <nbahadoor@gmail.com>
2017-01-04 20:49:07 +0000

    Adding Chapter 8, Learn How To Use Intersect Function With Examples Tutorial

---
+++ source-code/allaboutscala/src/main/scala/com/allaboutscala/chapter/eight/Intersect_Tutorial.scala
@@ -0,0 +1,47 @@
+package com.allaboutscala.chapter.eight
+
+/**
+  * Created by Nadim Bahadoor on 28/06/2016.
+  *
+  *  Tutorial: Learn How To Use Intersect Function With Examples
+  *
+  * [[http://allaboutscala.com/tutorials/chapter-8-beginner-tutorial-using-scala-collection-functions/scala-intersect-example/ Tutorial]]
+  *
+  * Copyright 2016 Nadim Bahadoor (http://allaboutscala.com)
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  * use this file except in compliance with the License. You may obtain a copy of
+  * the License at
+  *
+  *  [http://www.apache.org/licenses/LICENSE-2.0]
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+  * License for the specific language governing permissions and limitations under
+  * the License.
+  */
+object Intersect_Tutorial extends App {
+
+  println("Step 1: How to initialize a Set of donuts")
+  val donuts1: Set[String] = Set("Plain Donut", "Strawberry Donut", "Glazed Donut")
+  println(s"Elements of donuts1 = $donuts1")
+
+
+
+  println("\nStep 2: How to initialize another Set of donuts")
+  val donuts2: Set[String] = Set("Plain Donut", "Chocolate Donut", "Vanilla Donut")
+  println(s"Elements of donuts2 = $donuts2")
+
+
+
+  println("\nStep 3: How to find the common elements between two Sets using intersect function")
+  println(s"Common elements between donuts1 and donuts2 = ${donuts1 intersect donuts2}")
+  println(s"Common elements between donuts2 and donuts1 = ${donuts2 intersect donuts1}")
+
+
+
+  println("\nStep 4: How to find the common elements between two Sets using & function")
+  println(s"Common elements between donuts1 and donuts2 = ${donuts1 & donuts2}")
+  println(s"Common elements between donuts2 and donuts1 = ${donuts2 & donuts1}")
+}
