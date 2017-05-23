b0ae32d27b1e3fb9461dbe8f33e552d914cd4f86
Nadim Bahadoor <nbahadoor@gmail.com>
2017-05-23 21:52:58 +0100

    Chapter 5, Adding Traits, Companion Object, Factory Pattern

---
+++ source-code/allaboutscala/src/main/scala/com/allaboutscala/chapter/five/TraitFactory_Tutorial.scala
@@ -0,0 +1,73 @@
+package com.allaboutscala.chapter.five
+
+/**
+  * /**
+  * Created by Nadim Bahadoor on 28/06/2016.
+  *
+  *  Tutorial: Traits, Companion Object, Factory Pattern
+  *
+  * [[http://allaboutscala.com/tutorials/chapter-5-traits/scala-traits-companion-object-factory-pattern/]]
+  *
+  * Copyright 2016 Nadim Bahadoor (http://allaboutscala.com)
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  * use this file except in compliance with the License. You may obtain a copy of
+  * the License at
+  *
+  *  [http://www.apache.org/licenses/LICENSE-2.0]
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+  * License for the specific language governing permissions and limitations under
+  * the License.
+  */
+  */
+object TraitFactory_Tutorial extends App {
+
+
+  // Step 1: Define a wrapper object called Cakes to hold various types of cakes
+  object Cakes {
+
+    // Step 2: Define a base trait to represent a Cake
+    trait Cake {
+      def name: String
+    }
+
+    // Step 3: Define class implementations for the Cake trait namely: Cupcake, Donut and UnknownCake
+    class UnknownCake extends Cake {
+      override def name: String = "Unknown Cake ... but still delicious!"
+    }
+
+    class Cupcake extends Cake {
+      override def name: String = "Cupcake"
+    }
+
+    class Donut extends Cake {
+      override def name: String = "Donut"
+    }
+
+  }
+
+
+
+  // Step 4: Define a wrapper object called CakeFactory")
+  object CakeFactory {
+    import Cakes._
+
+    // Step 5: Define an apply method which will act as a factory to produce the correct Cake implementation
+    def apply(cake: String): Cake = {
+      cake match {
+        case "cupcake" => new Cupcake
+        case "donut" => new Donut
+        case _ => new UnknownCake
+      }
+    }
+  }
+
+
+  // Step 6: Call the CakeFactory
+  println(s"A cupcake = ${CakeFactory("cupcake").name}")
+  println(s"A donut = ${CakeFactory("donut").name}")
+  println(s"Unknown cake = ${CakeFactory("coconut tart").name}")
+}
