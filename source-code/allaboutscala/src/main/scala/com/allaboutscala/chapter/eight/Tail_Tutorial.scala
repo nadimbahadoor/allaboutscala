1cc9333bf408a5edeabfc53811ae158497e29365
Nadim Bahadoor <nbahadoor@gmail.com>
2017-12-14 21:05:35 +0000

    Chapter 8: Learn How To Use Tail Function With Examples

---
+++ source-code/allaboutscala/src/main/scala/com/allaboutscala/chapter/eight/Tail_Tutorial.scala
@@ -0,0 +1,45 @@
+package com.allaboutscala.chapter.eight
+
+
+/**
+  * Created by Nadim Bahadoor on 28/06/2016.
+  *
+  *  Tutorial: Learn How To Use Tail Function With Examples
+  *
+  * [[http://allaboutscala.com/tutorials/chapter-8-beginner-tutorial-using-scala-collection-functions/scala-tail-example/ Tutorial]]
+  *
+  * Copyright 2016 Nadim Bahadoor (http://allaboutscala.com)
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  * use this file except in compliance with the License. You may obtain a copy of
+  * the License at
+  *
+  *  [http://www.apache.org/licenses/LICENSE-2.0]
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+  * License for the specific language governing permissions and limitations under
+  * the License.
+  */
+object Tail_Tutorial extends App {
+
+  println("Step 1: How to initialize a Sequence of donuts")
+  val donuts: Seq[String] = Seq("Plain Donut", "Strawberry Donut", "Glazed Donut")
+  println(s"Elements of donuts = $donuts")
+
+
+
+  println("\nStep 2: How to return all elements in the sequence except the head using the tail function")
+  println(s"Elements of donuts excluding the head = ${donuts.tail}")
+
+
+
+  println("\nStep 3: How to access the last element of the donut sequence by using the last function")
+  println(s"Last element of donut sequence = ${donuts.last}")
+
+
+
+  println("\nStep 4: How to access the first element of the donut sequence by using the head function")
+  println(s"First element of donut sequence = ${donuts.head}")
+}
