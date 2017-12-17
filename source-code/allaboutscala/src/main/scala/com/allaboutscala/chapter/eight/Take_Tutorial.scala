23463c5ce28951e80e61cc34bcf865614b4c72e4
Nadim Bahadoor <nbahadoor@gmail.com>
2017-12-17 21:04:30 +0000

    Chapter 8: Learn How To Use Take Function With Examples

---
+++ source-code/allaboutscala/src/main/scala/com/allaboutscala/chapter/eight/Take_Tutorial.scala
@@ -0,0 +1,36 @@
+package com.allaboutscala.chapter.eight
+
+/**
+  * Created by Nadim Bahadoor on 28/06/2016.
+  *
+  *  Tutorial: Learn How To Use Take Function With Examples
+  *
+  * [[http://allaboutscala.com/tutorials/chapter-8-beginner-tutorial-using-scala-collection-functions/scala-take-example/ Tutorial]]
+  *
+  * Copyright 2016 Nadim Bahadoor (http://allaboutscala.com)
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  * use this file except in compliance with the License. You may obtain a copy of
+  * the License at
+  *
+  *  [http://www.apache.org/licenses/LICENSE-2.0]
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+  * License for the specific language governing permissions and limitations under
+  * the License.
+  */
+object Take_Tutorial extends App {
+
+  println("Step 1: How to initialize a Sequence of donuts")
+  val donuts: Seq[String] = Seq("Plain Donut", "Strawberry Donut", "Glazed Donut")
+  println(s"Elements of donuts = $donuts")
+
+
+
+  println("\nStep 2: How to take elements from the sequence using the take function")
+  println(s"Take the first donut element in the sequence =  ${donuts.take(1)}")
+  println(s"Take the first and second donut elements in the sequence =  ${donuts.take(2)}")
+  println(s"Take the first, second and third donut elements in the sequence =  ${donuts.take(3)}")
+}
