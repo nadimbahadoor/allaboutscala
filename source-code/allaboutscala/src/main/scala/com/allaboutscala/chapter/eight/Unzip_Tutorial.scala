6a94b9bd3093652a5fc662a503260c89f0cd3509
Nadim Bahadoor <nbahadoor@gmail.com>
2018-03-04 21:17:51 +0000

    Chapter 8: Learn How To Use Unzip Function With Examples

---
+++ source-code/allaboutscala/src/main/scala/com/allaboutscala/chapter/eight/Unzip_Tutorial.scala
@@ -0,0 +1,48 @@
+package com.allaboutscala.chapter.eight
+
+/**
+  * Created by Nadim Bahadoor on 28/06/2016.
+  *
+  *  Tutorial: Learn How To Use Unzip Function With Examples
+  *
+  * [[http://allaboutscala.com/tutorials/chapter-8-beginner-tutorial-using-scala-collection-functions/scala-unzip-example/ Tutorial]]
+  *
+  * Copyright 2016 Nadim Bahadoor (http://allaboutscala.com)
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  * use this file except in compliance with the License. You may obtain a copy of
+  * the License at
+  *
+  *  [http://www.apache.org/licenses/LICENSE-2.0]
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+  * License for the specific language governing permissions and limitations under
+  * the License.
+  */
+object Unzip_Tutorial extends App {
+
+  println("Step 1: How to initialize a Sequence of donuts")
+  val donuts: Seq[String] = Seq("Plain Donut", "Strawberry Donut", "Glazed Donut")
+  println(s"Elements of donuts = $donuts")
+
+
+
+  println("\nStep 2: How to initialize a Sequence of donut prices")
+  val donutPrices = Seq[Double](1.5, 2.0, 2.5)
+  println(s"Elements of donut prices = $donutPrices")
+
+
+
+  println("\nStep 3: How to zip the donuts Sequence with their corresponding prices")
+  val zippedDonutsAndPrices: Seq[(String, Double)] = donuts zip donutPrices
+  println(s"Zipped donuts and prices = $zippedDonutsAndPrices")
+
+
+
+  println("\nStep 4: How to unzip the zipped donut sequence into separate donuts names and prices Sequences")
+  val unzipped: (Seq[String], Seq[Double]) = zippedDonutsAndPrices.unzip
+  println(s"Donut names unzipped = ${unzipped._1}")
+  println(s"Donut prices unzipped = ${unzipped._2}")
+}
