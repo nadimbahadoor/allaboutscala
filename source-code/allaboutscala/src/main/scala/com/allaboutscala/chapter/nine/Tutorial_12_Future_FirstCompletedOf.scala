2c9b1f943d74ff6667b30eccf5089def93933d49
Nadim Bahadoor <nbahadoor@gmail.com>
2018-04-12 21:22:47 +0100

    Chapter 9: Future firstCompletedOf

---
+++ source-code/allaboutscala/src/main/scala/com/allaboutscala/chapter/nine/Tutorial_12_Future_FirstCompletedOf.scala
@@ -0,0 +1,55 @@
+package com.allaboutscala.chapter.nine
+
+import scala.util.{Success, Failure}
+
+/**
+  * Created by Nadim Bahadoor on 28/06/2016.
+  *
+  *  Tutorial: Future firstCompletedOf
+  *
+  * [[http://allaboutscala.com/tutorials/chapter-9-beginner-tutorial-using-scala-futures/#future-firstCompletedOf Tutorial]]
+  *
+  * Copyright 2016 Nadim Bahadoor (http://allaboutscala.com)
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  * use this file except in compliance with the License. You may obtain a copy of
+  * the License at
+  *
+  *  [http://www.apache.org/licenses/LICENSE-2.0]
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+  * License for the specific language governing permissions and limitations under
+  * the License.
+  */
+object Tutorial_12_Future_FirstCompletedOf extends App {
+
+  println("Step 1: Define a method which returns a Future Option")
+  import scala.concurrent.ExecutionContext.Implicits.global
+  import scala.concurrent.Future
+  def donutStock(donut: String): Future[Option[Int]] = Future {
+    println("checking donut stock")
+    if(donut == "vanilla donut") Some(10) else None
+  }
+
+
+
+  println(s"\nStep 2: Create a List of future operations")
+  val futureOperations = List(
+    donutStock("vanilla donut"),
+    donutStock("plain donut"),
+    donutStock("chocolate donut"),
+    donutStock("vanilla donut")
+  )
+
+
+  println(s"\nStep 3: Call Future.firstCompletedOf to get the results of the first future that completes")
+  val futureFirstCompletedResult = Future.firstCompletedOf(futureOperations)
+  futureFirstCompletedResult.onComplete {
+    case Success(results) => println(s"Results $results")
+    case Failure(e)       => println(s"Error processing future operations, error = ${e.getMessage}")
+  }
+
+  Thread.sleep(3000)
+}
